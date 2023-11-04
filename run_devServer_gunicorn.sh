46f43cf2edc109465622f6f169320541a2c4a9aa
Luis Pedro Coelho <luis@luispedro.org>
2023-11-04 16:02:17 +1000

    RFCT Remove internal (helper) scripts

--- run_devServer_gunicorn.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sudo env "PATH=$PATH" gunicorn src.main:app --workers 4 --threads 4  --worker-class uvicorn.workers.UvicornWorker -b 0.0.0.0:1010 --access-logfile '-' -t 120 --keep-alive 30 --reload
--- run_prodServer_gunicorn.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-sudo env PATH=$PATH gunicorn src.main:app --workers 4  --worker-class uvicorn.workers.UvicornWorker -b 0.0.0.0:1010 --access-logfile '-' -t 120
