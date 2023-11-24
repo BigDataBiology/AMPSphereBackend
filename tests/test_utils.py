74999ae12dd82d5cb1de2c5dffe76b81ac36940d
Luis Pedro Coelho <luis@luispedro.org>
2023-11-24 17:52:40 +1000

    BUG Fix test code

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 from src import utils
-def test_get_secondary_structure()
+def test_get_secondary_structure():
     assert utils.get_secondary_structure("AAAAAAAA") == {'helix': 0.0, 'turn': 0.0, 'sheet': 1.0}
     assert utils.get_secondary_structure("AAAAAAAACCCCCCCCCCCC") == {'helix': 0.0, 'turn': 0.0, 'sheet': 0.4}
     assert utils.get_secondary_structure("AAAAAAAACCCCCCCCCCCCWWWWW") == {'helix': 0.2, 'turn': 0.0, 'sheet': 0.32}
