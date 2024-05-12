49f3b1193650b212d7b55adb56da16d7dccaf214
Luis Pedro Coelho <luis@luispedro.org>
2024-05-12 16:40:57 +1000

    ENH Check for invalid microbial_source

--- src/crud.py
+++ src/crud.py
@@ -141,9 +141,13 @@ def get_families(page: int, page_size: int, habitat=None, microbial_source=None,
     if habitat is not None:
         query + 'general_envo_name == @habitat'
     if microbial_source is not None:
-        rank = database.gtdb_taxon_to_rank[microbial_source]
-        if query: query += ' and '
-        query += f'{rank} == @microbial_source'
+        try:
+            rank = database.gtdb_taxon_to_rank[microbial_source]
+        except KeyError:
+            gmsc_metadata = gmsc_metadata.iloc[:0]
+        else:
+            if query: query += ' and '
+            query += f'{rank} == @microbial_source'
     if sample is not None:
         if query: query += ' and '
         query += f'sample == @sample'
