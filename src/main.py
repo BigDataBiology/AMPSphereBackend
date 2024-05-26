24c7da9cea4d28dd85ea73b383faae70581c0416
Luis Pedro Coelho <luis@luispedro.org>
2024-05-26 15:33:03 +1000

    BUG Do not import outdated name

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.database import engine
 from src.router import amp_router, family_router, default_router
 import os
 
